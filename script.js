dd9cfcd8c47d95a223f7781b62dad05c022633ce
Mahelaka29 <117342536+Mahelaka29@users.noreply.github.com>
2023-02-19 19:08:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+// Cart Data Store
+
+let removeCartItemBtn = document.getElementsByClassName('danger-btn')
+console.log(removeCartItemBtn)
+for(let i = 0; i < removeCartItemBtn.length; i++){
+    let button = removeCartItemBtn[i]
+    button.addEventListener('click', function(event){
+        let buttonClicked = event.target
+        buttonClicked.closest("tr").remove()
+        updateCartTotal()
+    })
+}
+
+function updateCartTotal(){
+var cartItemContainer = document.getElementsByTagName('tbody')[0]
+var cartRows = cartItemContainer.document.getElementsByTagName('tbody')
+for(let i = 0; i < cartRows.length; i++){
+    var cartRow = cartRows[i]
+    var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+    var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+    console.log(priceElement, quantityElement)
+}
+}
